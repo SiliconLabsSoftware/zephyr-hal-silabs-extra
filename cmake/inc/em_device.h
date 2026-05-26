41830b4c07483aea63fe9895bb65a28b16adf2e2
Aksel Skauge Mellbye <aksel.mellbye@silabs.com>
2026-05-26 12:47:10 +0200

    zephyr: Add CMake build for crypto libraries
    Add build system to enable building crypto libraries from the
    sources in this repo.
    
    Signed-off-by: Aksel Skauge Mellbye <aksel.mellbye@silabs.com>

---
+++ cmake/inc/em_device.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2026 Silicon Laboratories Inc.
+ * SPDX-License-Identifier: Zlib
+ * 
+ * Minimal em_device.h for compilation of libcryptosoc.
+ */
+#ifndef EM_DEVICE_H
+#define EM_DEVICE_H
+
+/* xG22 and xG27 share a base address for CRYPTOACC */
+#define CRYPTOACC_BASE (0x5C020000UL)
+
+#endif
